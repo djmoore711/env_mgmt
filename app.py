b045dc9b0762fe076640695a52f4890fcc20f1c6
DJ Moore <djmoore7.11@gmail.com>
2025-02-24 23:10:26 -0600

    Add pseudocode for project setup and virtual environment creation to app.py

--- app.py
+++ app.py
@@ -1,2 +1,24 @@
 # app.py
-print("Hello, World!")
+
+'''
+PSUEDO CODE TIME!
+'''
+
+'''
+
+Creation of new projects and virtual environments
+    # Set the project name
+    # Create the project directory
+    # Setup the virtual environment
+    # Activate the virtual environment
+    # Creating application files
+    # Creating requirements.txt
+    # Creating gitignore file
+    # Environment variables
+    # MacOS system files
+    # Inform the user the new project setup is complete
+
+Change environments easily by using the project name
+
+
+'''
